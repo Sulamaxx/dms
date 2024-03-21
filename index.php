a23c43232e8f41a5e239465f0522e95e5a1861ea
Sulamaxx <115876513+Sulamaxx@users.noreply.github.com>
2024-03-21 16:58:00 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DMS Store</title>
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/login/logo.png">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+</head>
+
+<body class="index_body vh-100">
+
+    <div class="container-fluid d-flex justify-content-center align-items-center index_container">
+        <div class="row">
+            <div class="col-lg-6 col-12 p-5">
+                <div class="col-12 mb-3">
+                    <div class="row d-flex justify-content-center align-content-center">
+                        <img class="col-4" src="assets/img/login/logo.png" alt="">
+                    </div>
+                </div>
+                <div class="col-12 p-4">
+                    <span class="fs-3 fw-bold">Login</span>
+                </div>
+
+                <div class="col-12">
+                    <div class="row">
+                        <div class="input-group mb-3 col-12">
+                            <span class="input-group-text" id="basic-addon1"><img width="50px" src="assets/img/login/user_person_profile_avatar_people_icon.png" alt=""></span>
+                            <input type="text" class="form-control fs-4" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <div class="row">
+                        <div class="input-group mb-3 col-12">
+                            <span class="input-group-text" id="basic-addon1"><img width="50px" src="assets/img/login/protect_safety_icon.png" alt=""></span>
+                            <input type="password" class="form-control fs-4" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <div class="row">
+                        <Button class="btn fs-2 fw-bold index_login_btn">Log In</Button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-12 index_side_image_container">
+                <div class="row justify-content-center">
+                    <img class="col-10 index_side_image" src="assets/img/login/cashier_image.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 mt-3">
+        <div class="row">
+            <span class="text-white fs-5 text-center">&copy; <script>
+                    new Date().getFullYear
+                </script> KODFEC Pvt Ltd || All Right Reserved</span>
+        </div>
+    </div>
+    <script src="assets/js/script.js"></script>
+</body>
+
+</html>
